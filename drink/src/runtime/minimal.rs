b2be547afb898418cd132098d39c1b7c37356595
Andrew Jones <ascjones@gmail.com>
2023-11-23 13:01:43 +0000

    Adapt to local riscv pallet-contracts

--- drink/src/runtime/minimal.rs
+++ drink/src/runtime/minimal.rs
@@ -76,6 +76,7 @@ mod construct_runtime {
         type MaxHolds = ConstU32<1>;
         type MaxFreezes = ();
         type RuntimeHoldReason = RuntimeHoldReason;
+        type RuntimeFreezeReason = RuntimeFreezeReason;
     }
 
     // ------------ Configure pallet timestamp -----------------------------------------------------
